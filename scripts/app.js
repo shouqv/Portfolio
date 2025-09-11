d569f64468a1bd32dc6c002ea29b18ab6ecf4e7f
Shouq Almutairi <shouqalmutaiiri8@gmail.com>
2025-09-12 00:37:36 +0300

    editting the skill animation :)

--- scripts/app.js
+++ scripts/app.js
@@ -5,11 +5,13 @@ function init() {
   let navPosition = -10;
 
   let intersectedFlags = [];
+  let divMoved = false;
 
   let flashlight = document.getElementById("flashlight");
   let mainText = document.querySelectorAll(".inMain")
+  let movingDiv = document.querySelector(".movingDiv")
   // let movingText = document.querySelector(".movingText")
-  // console.log(movingText)
+  console.log(movingDiv)
 
 
 
@@ -37,67 +39,75 @@ function init() {
 
     flashlight.style.setProperty("--Xpos", mouseX + "px");
     flashlight.style.setProperty("--Ypos", mouseY + "px");
-  // end of flashlight effect
+    // end of flashlight effect
   }
 
 
-      let observer = new IntersectionObserver(function (entries) {
-      entries.forEach(function (entry) {
-        if (entry.isIntersecting && !intersectedFlags.find((element) => element === entry.target)) {
-          
-          let opacity = 0
-          for (let i = 0; i <=100; i++) {
-            setTimeout(() => {
-              opacity += 0.01;
-              entry.target.style.color = `rgba(224, 224, 224, ${opacity}`;
-              
-            }, i * 10);
-          }
+  let observer = new IntersectionObserver(function (entries) {
+    entries.forEach(function (entry) {
+      if (entry.isIntersecting && !intersectedFlags.find((element) => element === entry.target)) {
 
+        let opacity = 0
+        for (let i = 0; i <= 100; i++) {
+          setTimeout(() => {
+            opacity += 0.01;
+            entry.target.style.color = `rgba(224, 224, 224, ${opacity}`;
 
-          // console.log(entry.target)
-          intersectedFlags.push(entry.target)
+          }, i * 10);
         }
-      });
-    });
 
-    mainText.forEach(element =>
+        if (entry.target.innerText === "skills" && !divMoved) {
+
+          //crediting the idea from:
+          //https://www.w3schools.com/jsref/tryit.asp?filename=tryjsref_style_animation
+
+          movingDiv.style.animation = "riseFade 4s both";
+          
+
+        }
+
+
+       
+
+        
+        console.dir(entry.target)
+        intersectedFlags.push(entry.target)
+
+      }
+    });
+  });
+
+  mainText.forEach(element =>
     observer.observe(element))
 
 
 
+  //   let observer2 = new IntersectionObserver(function (entries) {
+  //     console.log("imhere")
+  //   entries.forEach(function (entry) {
+  //     if (entry.isIntersecting && !divMoved ) {
+  //       let leftPosition = -100
+  //       for (let i = 0; i <=150; i++) {
+  //         setTimeout(() => {
+  //           leftPosition += 1;
+  //           entry.target.style.left = `${leftPosition}%`;
+  //         }, i * 10);
+  //       }
+  //       console.log("imhere")
+  //       divMoved = true;
+  //     }
+  //   });
+  // });
 
-      
-
-
-
-
-    // let observer2 = new IntersectionObserver(function (entries) {
-    //   entries.forEach(function (entry) {
-    //     let leftPosition = -100
-    //     let sec =1;
-    //     while (entry.isIntersecting ) {
-    
-          
-          
-    //         setTimeout(() => {
-    //           leftPosition += 1;
-    //           entry.target.style.left=`${leftPosition}px`;
-              
-    //         }, 1000);
-   
-    //       // console.log(entry.target)
-    //       intersectedFlags.push(entry.target)
-    //     }
-    //   });
-    // });
+  // observer2.observe(movingDiv)
 
 
-    
-      // observer2.observe(movingText)
-    
 
-  
+
+
+
+
+
 
   flashlight.addEventListener("mousemove", getMousePosition);
   flashlight.addEventListener("touchmove", getMousePosition);
